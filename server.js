97349b3e9b6103b8d9a31a336a2d35b2e6d921be
Jiaping Zeng <jzeng@outlook.com>
2017-02-04 17:27:34 -0800

    test

--- server.js
+++ server.js
@@ -66,7 +66,8 @@ function sendTextMessage(recipientId, messageText) {
         },
         message: {
             text: messageText
-        }
+        },
+        sender_action: "typing_on"
     }
     //console.log('sending "' + messageText + '"')
     callSendAPI(messageData)
