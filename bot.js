f73f5b5395218a9f89902adaa1fd93db23aa938c
Jiaping Zeng <jzeng@outlook.com>
2017-02-04 16:35:43 -0800

    oiafsd

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ module.exports = {
     respond: function(message) {
         // TO-DO: improve this ghetto logic
         switch (message.toLowerCase()) {
+            case "insult":
             case "insult me":
             case "idiot":
                 return randomMessage(response.insult)
