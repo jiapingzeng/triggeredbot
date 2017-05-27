87bf637230f32c11be79f16bf6bf46b83639fb3f
Jiaping Zeng <jzeng@outlook.com>
2017-05-26 23:28:40 -0700

    plz no break

--- bot.js
+++ bot.js
@@ -1,69 +1,39 @@
-const knowledge = require('./knowledge.json')
+var knowledge = require('./knowledge')
 
-module.exports = {
-    respond: function (message) {        
-        switch (message.toLowerCase()) {
-            case "insult":
-            case "insult me":
-            case "idiot":
-                return randomMessage(knowledge.insult.responses)
-            case 'hi':
-            case 'hello':
-            case 'hey':
-            case 'yo':
-            case 'sup':
-                return randomMessage(knowledge.greeting.responses)
-            case 'joke':
-            case 'tell me a joke':
-                return randomMessage(knowledge.joke.responses)
-            default:
-                return randomMessage(knowledge.unknown.responses)
-        }
-        /*for (var type in knowledge) {
-        // for (var i = 0; i < knowledge.length; i++) {
-            //var type = knowledge[i]
-            console.log(type)
-            console.log(type.triggers)
-            for (var trigger in type.triggers) {
-            //for (var j = 0; j < type[0].length; j++) {
-                //var trigger = type[0][j]
+var bot = {
+  respond: function(message) {
+    var response = randomMessage(knowledge.unknown.responses)
+    forEach(knowledge, function(type, next) {
+        var triggers = type.triggers
+        for (var i = 0; i < triggers.length; i++) {
+            var trigger = triggers[i]
+            if (message.includes(trigger)) {
                 console.log(trigger)
-                if (trigger.contains(' ') && message.contains(trigger)) {
-                    return randomMessage(type.responses)
-                } else if (message.contains(trigger)) {
-                    var start = message.indexOf(trigger)
-                    var end = message.indexOf(trigger) + trigger.length
-
-                    // wtf is this
-                    if (start > 0) {
-                        if (message.charAt(start - 1) != ' ') {
-                            if (end < message.length - 1) {
-                                if (message.charAt(end + 1) != ' ') {
-                                    return randomMessage(type.responses)
-                                }
-                            }
-                            else {
-                                return randomMessage(type.responses)
-                            }
-                        }
-                    } else if (end < message.length - 1) {
-                        if (message.charAt(end + 1) != ' ') {
-                            return randomMessage(type.responses)
-                        }
-                    }
-                    else {
-                        return randomMessage(type.responses)
-                    }
-                    // wtf was that
-                }
+                response = randomMessage(type.responses)
             }
         }
-        return randomMessage(knowledge.unknown.responses)*/
-    }
+        next()
+    })
+    return response
+  }
 }
 
-function randomMessage(messages) {
+var randomMessage = function(messages) {
     if (messages) {
         return messages[Math.floor(Math.random() * messages.length)]
     }
 }
+
+var forEach = function(o, cb) {
+    var i = 0
+    var keys = Object.keys(o)
+    var length = keys.length
+    var next = function() {
+        if(i < length) {
+            cb(o[keys[i++]], next)
+        }
+    }
+    next()
+}
+
+module.exports = bot
